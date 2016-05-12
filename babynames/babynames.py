18600dd99992d6ff83fe277b845a502687e80f25
Kimberly Lin <kimberlylin90@gmail.com>
2016-05-12 18:27:56 -0400

    scripting exercises

--- babynames/babynames.py
+++ babynames/babynames.py
@@ -41,7 +41,49 @@ def extract_names(filename):
   ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
   """
   # +++your code here+++
-  return
+
+  # Build the [year, 'name rank', ... ] list and print it
+  names = []
+
+  f = open(filename, 'rU')
+  text = f.read()
+  f.close()
+
+  # Extract the year and print it
+  match_year = re.search(r'Popularity\sin\s(\d\d\d\d)', text)
+  if match_year:
+    year = match_year.group(1)
+    # print year
+    names.append(year)
+
+  # Extract the names and rank numbers and just print them
+  # match this --> <tr align="right"><td>1</td><td>Michael</td><td>Jessica</td>
+  match_names = re.findall(r'<tr\salign="right"><td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>', text)
+  # if match_names:
+    # print match_names # ('233', 'Theodore', 'Adrienne')
+    # print match_names.group(0)
+    # print match_names.group(1)
+    # print match_names.group(2)
+    # print match_names.group(3)
+
+  # Get the names data into a dict and print it, sort it in alphabetical order
+  name_rank_dict = {}
+  for rank, boy_name, girl_name in match_names:
+    # print x[0], x[1], x[2]
+    # print rank, boy_name, girl_name
+    
+    if boy_name not in name_rank_dict:
+      name_rank_dict[boy_name] = rank
+    if girl_name not in name_rank_dict:
+      name_rank_dict[girl_name] = rank
+
+  sorted_names = sorted(name_rank_dict.keys())
+
+  for name in sorted_names:
+    # print name, name_rank_dict[name]
+    names.append(name + " " + name_rank_dict[name])
+
+  return names
 
 
 def main():
@@ -63,6 +105,17 @@ def main():
   # +++your code here+++
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
+  for filename in args:
+    names = extract_names(filename)
+
+    text = '\n'.join(names)
+
+    if summary:
+      outf = open(filename + '.summary', 'w')
+      outf.write(text)
+      outf.close()
+    else:
+      print text
   
 if __name__ == '__main__':
   main()
